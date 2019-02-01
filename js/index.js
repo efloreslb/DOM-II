b44535a87ea7f2d856b63f7db9c23cb1dec557eb
Edgar F <eflores.ef@gmail.com>
2019-01-31 21:38:39 -0800

    Completed two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+    link.addEventListener('mouseover', (event) => {
+        link.style.color = "green";
+    })
+})
+
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+//NOT WORKING
+let h1 = document.querySelector('.logo-heading');
+h1.addEventListener('keypress', (event) => {
+    h1.style.color = 'red';
+})
+
