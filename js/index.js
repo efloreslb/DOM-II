b8afe66b02b996746e42900345c028874037e1dd
Edgar F <eflores.ef@gmail.com>
2019-02-03 20:25:37 -0800

    Added Stop Propagation effect

--- js/index.js
+++ js/index.js
@@ -1,18 +1,42 @@
 // Your code goes here
 
-// mouseover - WORKS
-let navLinks = document.querySelectorAll('.nav-link');
+//Propagation
 
+let destination = document.querySelectorAll('.destination');
+destination.forEach((place) => {
+    place.addEventListener('click', (event) => {
+        place.style.backgroundColor = "red";
+    })
+})
+
+let h4 = document.querySelectorAll('h4');
+h4.forEach((title) => {
+    title.addEventListener('click', (event) => {
+        title.style.backgroundColor = "green";
+        event.stopPropagation(); // <-- Stop Propagation of h4 effect
+    })
+})
+
+//Event Listeners
+
+
+
+// 1 mouseover - WORKS
+let navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach((link) => {
     link.addEventListener('mouseover', (event) => {
-        link.style.color = "green";
-        link.style.backgroundColor = "lightgray"
-        link.style.width = "80px"
+        link.style.color = "white";
+        link.style.backgroundColor = "gray"
         link.style.verticalAlign = "middle";
     })
 })
 
-//mouseout - WORKS
+let img = document.querySelectorAll('img');
+img[0].addEventListener('mouseover', event => {
+    img[0].setAttribute('src', 'img/fly-bus.png');
+})
+
+// 2 mouseout - WORKS
 
 navLinks.forEach((link) => {
     link.addEventListener('mouseout', (event) => {
@@ -21,9 +45,12 @@ navLinks.forEach((link) => {
     })
 })
 
-// click - WORKS
+img[0].addEventListener('mouseout', event => {
+    img[0].setAttribute('src', 'img/fun-bus.jpg');
+})
 
-let img = document.querySelectorAll('img');
+// 3 click - WORKS
+
 img[1].addEventListener('click', (event) => {
     img[1].setAttribute('src', 'img/kitty01.png')
 })
@@ -31,7 +58,7 @@ img[2].addEventListener('click', (event) => {
     img[2].setAttribute('src', 'img/kitty01.png')
 })
 
-//dblclick - WORKS
+// 4 dblclick - WORKS
 
 img[1].addEventListener('dblclick', (event) => {
     img[1].setAttribute('src', 'img/kitty02.png')
@@ -41,56 +68,55 @@ img[2].addEventListener('dblclick', (event) => {
     img[2].setAttribute('src', 'img/kitty02.png')
 })
 
-// resize - WORKS
+// 5 resize - WORKS
 
 let h2 = document.querySelectorAll('h2');
 window.addEventListener('resize', (event) => {
     h2.forEach((heading) => {
-        heading.style.color= "blue";
+        heading.style.color= "steelblue";
         heading.style.fontSize= "50px";
+        heading.textContent = "Whoa resizing!"
     })
 })
 
-// scroll - WORKS
+// 6 scroll - WORKS
 
-window.addEventListener("scroll", (event) => {
-    img[0].setAttribute('src', 'img/fly-bus.png');
+let body = document.querySelector('body');
+let color = ['moccasin', 'navajowhite', 'palegoldenrod', 'papayawhip', 'peachpuff'];
+
+document.addEventListener("scroll", (event) => {
+    body.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
 })
 
-// load
+// 7 load - WORKS
 
 window.addEventListener("load", (event) => {
     alert("Welcome to the Fun Bus!");
 })
 
-// keydown
-//var randomColor = Math.floor(Math.random()*16777215).toString(16);
-//NOT WORKING
+// 8 keydown
 
-let container = querySelector('.container');
-container.addEventListener('keydown', (event) => {
-    container.style.backgroundColor = "yellow";
+let h3 = document.querySelectorAll('h3');
+    h3.forEach((heading) => {
+        heading.addEventListener('keydown', e => {
+        heading.textContent = "keydown";
+    })
 })
 
-// drag / drop
+
+// 9 drag / drop
 
 let h1 = querySelector('h1');
 h1.addEventListener('drag', (event) => {
     h1.style.color = "pink";
 })
 
-// focus
-
-// select
+// 10 select
 
 let p = querySelectorAll('p');
-p.addEventListener('select', (event) => {
-    p.style.backgroundColor = "blue";
+p[0].addEventListener('select', (event) => {
+    p[0].style.textContent = `Ooh new paragraph!`;
 })
 
-//propagation
 
-let btn = querySelectorAll('.btn');
-btn[0].addEventListener('click', (event) => {
-    event.child.style.backgroundColor = "green";
-})
+
